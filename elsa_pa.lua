5acbc6549b280bc6ad18162a7b6aacbc86daa65b
Markus Stenberg <markus.stenberg@iki.fi>
2012-10-03 23:33:52 +0300

    Fleshed out elsa_pa. Now it probably mostly works.

--- elsa_pa.lua
+++ elsa_pa.lua
@@ -9,8 +9,8 @@
 --       All rights reserved
 --
 -- Created:       Wed Oct  3 11:47:19 2012 mstenber
--- Last modified: Wed Oct  3 17:21:33 2012 mstenber
--- Edit time:     7 min
+-- Last modified: Wed Oct  3 23:27:11 2012 mstenber
+-- Edit time:     50 min
 --
 
 -- the main logic around with prefix assignment within e.g. BIRD works
@@ -18,7 +18,11 @@
 -- elsa_pa is given skv instance, elsa instance, and should roll on
 -- it's way. 
 
+AC_TYPE=0x1234
+
 require 'mst'
+require 'codec'
+
 local pa = require 'pa'
 
 module(..., package.seeall)
@@ -26,11 +30,11 @@ module(..., package.seeall)
 elsa_pa = mst.create_class{class='elsa_pa', mandatory={'skv', 'elsa'}}
 
 function elsa_pa:init()
-   self.pa = pa.pa:new{rid='myrid'}
+   local rid = self.rid
+   self.pa = pa.pa:new{rid=rid, client=self}
 end
 
 function elsa_pa:uninit()
-
    -- we don't 'own' skv or 'elsa', so we don't do anything here,
    -- except clean up our own state
 
@@ -38,5 +42,99 @@ function elsa_pa:uninit()
 end
 
 function elsa_pa:run()
-   
+   local r = self.pa:run()
+   if r
+   then
+      self.elsa:originate_lsa{type=AC_TYPE, 
+                              rid=self.pa.rid,
+                              body=self:generate_ac_lsa()}
+      local t = mst.set:new()
+      for i, lap in ipairs(self.pa.lap:values())
+      do
+         -- XXX - map iid => ifname
+         t:insert({ifname=lap.iid, prefix=lap.prefix})
+      end
+      self.skv:set('ospf-lap', t)
+   end
+end
+
+function elsa_pa:iterate_ac_lsa(f, criteria)
+   if criteria
+   then
+      criteria = mst.table_copy(criteria)
+   else
+      criteria = {}
+   end
+   criteria.type = AC_TYPE
+   self.elsa:iterate_lsa(f, criteria)
+end
+
+function elsa_pa:iterate_ac_lsa_tlv(f, criteria)
+   function inner_f(lsa) 
+      for i, tlv in ipairs(codec.decode_ac_tlvs(lsa.body))
+      do
+         if not criteria or mst.table_contains(tlv, criteria)
+         then
+            f(tlv, lsa)
+         end
+      end
+   end
+   self:iterate_ac_lsa(inner_f)
+end
+
+--  iterate_rid(f) => callback with rid
+function elsa_pa:iterate_rid(f)
+   self.elsa:iterate_lsa(function (lsa) f(lsa.rid) end)
+end
+
+--  iterate_asp(f) => callback with prefix, iid, rid
+function elsa_pa:iterate_asp(f)
+   self:iterate_ac_lsa_tlv(function (asp, lsa) 
+                              self:a(lsa and asp)
+                              f(asp.prefix, asp.iid, lsa.rid)
+                           end, {type=codec.AC_TLV_ASP})
+end
+
+--  iterate_usp(f) => callback with prefix, rid
+function elsa_pa:iterate_usp(f)
+   self:iterate_ac_lsa_tlv(function (usp, lsa)
+                              self:a(lsa and usp)
+                              f(usp.prefix, lsa.rid)
+                           end, {type=codec.AC_TLV_USP})
+end
+
+--  iterate_if(rid, f) => callback with iid, highest_rid
+function elsa_pa:iterate_if(rid, f)
+   self.elsa:iterate_if(rid, function (iid, highest_rid)
+                           self:a(iid)
+                           f(iid, highest_rid)
+                        end)
+end
+
+function elsa_pa:generate_ac_lsa()
+   local a = mst.array:new()
+
+   -- generate USP-based TLVs
+   for i, ifname in ipairs(self.skv:get('iflist') or {})
+   do
+      local o = self.skv:get(string.format('pd.%s', ifname) )
+      if o
+      then
+         local prefix, valid = unpack(o)
+         if not valid or valid >= os.time()
+         then
+            a:insert(codec.usp_ac_tlv:encode{prefix=prefix})
+         end
+      end
+   end
+
+   -- generate (local) ASP-based TLVs
+   for i, asp in ipairs(self.pa:get_local_asp_values())
+   do
+      a:insert(codec.asp_ac_tlv:encode{prefix=asp.prefix, iid=asp.iid})
+   end
+   if #a
+   then
+      return table.concat(a)
+   end
 end
--- spec/elsa_pa_spec.lua
+++ spec/elsa_pa_spec.lua
@@ -9,8 +9,8 @@
 --       All rights reserved
 --
 -- Created:       Wed Oct  3 11:49:00 2012 mstenber
--- Last modified: Wed Oct  3 17:18:39 2012 mstenber
--- Edit time:     18 min
+-- Last modified: Wed Oct  3 23:26:44 2012 mstenber
+-- Edit time:     53 min
 --
 
 require 'mst'
@@ -21,17 +21,104 @@ require 'ssloop'
 
 delsa = mst.create_class{class='delsa'}
 
+function delsa:iterate_lsa(f, criteria)
+   for rid, body in pairs(self.lsas)
+   do
+      f{rid=rid, body=body}
+   end
+end
+
+function delsa:iterate_if(rid, f)
+   for i, v in ipairs(self.iid[rid] or {})
+   do
+      f(v)
+   end
+end
+
+function delsa:originate_lsa(lsa)
+   self:a(lsa.type == elsa_pa.AC_TYPE)
+   self.lsas[lsa.rid] = lsa.body
+end
+
 describe("elsa_pa", function ()
             it("can be created", function ()
-                  local e = delsa:new()
-                  local skv = skv.skv:new{long_lived=false, port=31337}
-                  local ep = elsa_pa.elsa_pa:new{elsa=e, skv=skv}
+                  local base_lsas = {r1=codec.usp_ac_tlv:encode{prefix='dead::/16'}}
+                  local e = delsa:new{iid={mypid={42, 123}}, 
+                                      lsas=base_lsas}
+                  local skv = skv.skv:new{long_lived=true, port=31337}
+                  local ep = elsa_pa.elsa_pa:new{elsa=e, skv=skv, rid='mypid'}
+
+                  -- run once, and make sure we get to pa.add_or_update_usp
+                  local usp_added = false
+                  local asp_added = false
+                  ssloop.inject_snitch(ep.pa, 'add_or_update_usp', function ()
+                                          usp_added = true
+                                                                end)
+                  ssloop.inject_snitch(ep.pa, 'add_or_update_asp', function ()
+                                          asp_added = true
+                                                                end)
                   ep:run()
+                  mst.a(usp_added)
+                  mst.a(not asp_added)
+
+                  asp_added = false
+                  usp_added = false
+                  ep:run(ep)
+                  mst.a(asp_added)
+                  mst.a(usp_added)
+
+                  -- cleanup
                   ep:done()
                   skv:done()
                   e:done()
+
+                  -- make sure cleanup really was clean
                   local r = ssloop.loop():clear()
                   mst.a(not r, 'event loop not clear')
 
                                  end)
+            it("2 sync state ok", function ()
+                  mst.d_xpcall(function ()
+
+                  local base_lsas = {r1=codec.usp_ac_tlv:encode{prefix='dead::/16'}}
+                  local e = delsa:new{iid={ep1={42, 123}, 
+                                           ep2={43,123}},
+                                      lsas=base_lsas}
+                  local skv1 = skv.skv:new{long_lived=true, port=31338}
+                  local skv2 = skv.skv:new{long_lived=true, port=31339}
+                  local ep1 = elsa_pa.elsa_pa:new{elsa=e, skv=skv1, rid='ep1'}
+                  local ep2 = elsa_pa.elsa_pa:new{elsa=e, skv=skv2, rid='ep2'}
+
+                  -- run once, and make sure we get to pa.add_or_update_usp
+
+                  for i=1,3
+                  do
+                     mst.d('running iter', i)
+                     ep1:run()
+                     ep2:run()
+                  end
+
+
+                  -- 3 asps -> each should have 3 asps + 2 lap
+                  -- (2 ifs per box)
+                  for i, ep in ipairs({ep1, ep2})
+                  do
+                     mst.a(ep.pa.asp:count() == 3)
+                     mst.a(ep.pa.lap:count() == 2)
+                  end
+
+                  -- cleanup
+                  ep1:done()
+                  ep2:done()
+                  skv1:done()
+                  skv2:done()
+
+                  e:done()
+
+                  -- make sure cleanup really was clean
+                  local r = ssloop.loop():clear()
+                  mst.a(not r, 'event loop not clear')
+
+                                 end)
+                    end)
                     end)
