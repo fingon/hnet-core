ea13bbea287fb92fdafaa3dab414e619c083f332
Markus Stenberg <markus.stenberg@iki.fi>
2012-11-13 14:20:46 +0200

    Added if_table to the dpm, and also get_ipv6_lap.

--- dpm.lua
+++ dpm.lua
@@ -8,8 +8,8 @@
 -- Copyright (c) 2012 cisco Systems, Inc.
 --
 -- Created:       Thu Nov  8 09:23:58 2012 mstenber
--- Last modified: Thu Nov  8 09:28:23 2012 mstenber
--- Edit time:     1 min
+-- Last modified: Tue Nov 13 13:58:36 2012 mstenber
+-- Edit time:     4 min
 --
 
 -- dummy version of pm_core's pm class
@@ -24,8 +24,13 @@ dpm = mst.create_class{class='dpm'}
 function dpm:init()
    self.ds = self.ds or dshell.dshell:new{}
    self.shell = self.shell or self.ds:get_shell()
+   self.if_table = linux_if.if_table:new{shell=self.shell} 
 end
 
 function dpm:get_ipv6_usp()
    return self.ipv6_usps or {}
+end
+
+function dpm:get_ipv6_lap()
+   return self.ipv6_laps or {}
 end
