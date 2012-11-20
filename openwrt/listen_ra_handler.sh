5733955da064a3f6463bd3b99e29781811552f3b
Markus Stenberg <markus.stenberg@iki.fi>
2012-11-20 17:16:17 +0200

    Added note about dridd handling still being bit inomplete. Added also rdisc6 call to get the RA-sourced default route up faster (may be specific to some Linux versions, but too lazy to study).

--- openwrt/listen_ra_handler.sh
+++ openwrt/listen_ra_handler.sh
@@ -8,8 +8,8 @@
 # Copyright (c) 2012 cisco Systems, Inc.
 #
 # Created:       Thu Nov  8 10:02:26 2012 mstenber
-# Last modified: Tue Nov 20 15:52:44 2012 mstenber
-# Edit time:     11 min
+# Last modified: Tue Nov 20 17:10:08 2012 mstenber
+# Edit time:     14 min
 #
 
 # Start or stop listening to ra default router info on apsecific interface
@@ -33,12 +33,13 @@ start() {
         echo 0 > /proc/sys/net/ipv6/conf/$IF/forwarding
     fi
 
-    # Setting it briefly to 0 will cause good things to happen, hopefully
-    echo 0 > /proc/sys/net/ipv6/conf/$IF/accept_ra
     echo 0 > /proc/sys/net/ipv6/conf/$IF/accept_ra_pinfo
-
-
     echo 2 > /proc/sys/net/ipv6/conf/$IF/accept_ra
+
+    # This is strictly optional package, but proactively doing rdisc6
+    # gets the information to kernel too much faster. If not available,
+    # no harm, no foul..
+    rdisc6 $IF > /dev/null &
 }
 
 stop() {
