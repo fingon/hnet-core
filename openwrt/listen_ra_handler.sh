0ba725497f05d7becde5e24d03cbe5d96cfd702e
Markus Stenberg <markus.stenberg@iki.fi>
2012-11-20 16:19:28 +0200

    Added pre-3.1 workaround to the listen_ra_handler.sh.

--- openwrt/listen_ra_handler.sh
+++ openwrt/listen_ra_handler.sh
@@ -8,8 +8,8 @@
 # Copyright (c) 2012 cisco Systems, Inc.
 #
 # Created:       Thu Nov  8 10:02:26 2012 mstenber
-# Last modified: Thu Nov  8 10:04:30 2012 mstenber
-# Edit time:     2 min
+# Last modified: Tue Nov 20 15:52:44 2012 mstenber
+# Edit time:     11 min
 #
 
 # Start or stop listening to ra default router info on apsecific interface
@@ -19,16 +19,34 @@
 # or
 # stop if
 
+
 start() {
     IF=$1
-    echo 2 > /proc/sys/net/ipv6/conf/$IF/accept_ra
+    KERN=`uname -r | awk -F. '{ printf("%d.%d\n",$1,$2); }'`
+    if [ "$KERN" \< "3.1" ]
+    then
+        # Rather ugly hack - set forwarding on _that_ interface to 0
+        # so it will accept RA's
+
+        # (It doesn't seem to affect actual forwarding, as long as
+        # all/forwarding > 0)
+        echo 0 > /proc/sys/net/ipv6/conf/$IF/forwarding
+    fi
+
+    # Setting it briefly to 0 will cause good things to happen, hopefully
+    echo 0 > /proc/sys/net/ipv6/conf/$IF/accept_ra
     echo 0 > /proc/sys/net/ipv6/conf/$IF/accept_ra_pinfo
+
+
+    echo 2 > /proc/sys/net/ipv6/conf/$IF/accept_ra
 }
 
 stop() {
     IF=$1
     echo 1 > /proc/sys/net/ipv6/conf/$IF/accept_ra
     echo 1 > /proc/sys/net/ipv6/conf/$IF/accept_ra_pinfo
+    # XXX - should we restore the forwarding flag here, or not? Sad
+    # thing is, we don't know what value it was at..
 }
 
 CMD=$1
