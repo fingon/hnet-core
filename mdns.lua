6fd7bb872f3a45fcb864b8e34b2f40413ea16c2b
Markus Stenberg <markus.stenberg@iki.fi>
2013-01-28 16:42:35 +0200

    Made v4 multicast group join optional, as it seems to be bit flaky.

--- mdns.lua
+++ mdns.lua
@@ -8,8 +8,8 @@
 -- Copyright (c) 2013 cisco Systems, Inc.
 --
 -- Created:       Sun Jan 27 12:38:01 2013 mstenber
--- Last modified: Mon Jan 28 15:20:59 2013 mstenber
--- Edit time:     18 min
+-- Last modified: Mon Jan 28 16:41:45 2013 mstenber
+-- Edit time:     21 min
 --
 
 -- 'mdns' daemon, which shares state (via skv and then via OSPF AC LSA
@@ -67,7 +67,12 @@ local ifindex=nil
 local mct6 = {multiaddr=mcast6, interface=ifindex}
 
 --checked_setoption(o.s, 'ipv6-v6only', true)
-checked_setoption(o.s, 'ip-add-membership', mct4)
+-- ignore if this fails too, v4 is just bonus, we mainly do v6
+local r, err = o.s:setoption('ip-add-membership', mct4)
+if not r
+then
+   self:d('ipv4 multicast group join failed', mct4, err)
+end
 checked_setoption(o.s, 'ipv6-add-membership', mct6)
 checked_setoption(o.s, 'ipv6-unicast-hops', 255)
 checked_setoption(o.s, 'ipv6-multicast-hops', 255)
