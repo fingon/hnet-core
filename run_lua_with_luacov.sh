a35d27b99e541f16e4226cbfb2da81558f972f43
Markus Stenberg <markus.stenberg@iki.fi>
2013-02-04 17:24:44 +0200

    By default not using luacov.tick, as busted _does_ terminate.. Dcoumented why.

--- run_lua_with_luacov.sh
+++ run_lua_with_luacov.sh
@@ -8,8 +8,12 @@
 # Copyright (c) 2012 cisco Systems, Inc.
 #
 # Created:       Tue Nov 13 13:17:58 2012 mstenber
-# Last modified: Tue Nov 13 13:24:06 2012 mstenber
-# Edit time:     1 min
+# Last modified: Mon Feb  4 16:13:15 2013 mstenber
+# Edit time:     2 min
 #
 
-exec lua -lluacov -lluacov.tick $*
+# Enable this if stuff doesn't terminate
+#exec lua -lluacov -lluacov.tick $*
+
+# With this, we write luacov stats just at the end
+exec lua -lluacov $*
