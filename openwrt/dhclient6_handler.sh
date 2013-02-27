6edf9a04c74fc6d2e114af77d0364c26409dc6b6
Markus Stenberg <markus.stenberg@iki.fi>
2013-02-27 13:54:12 +0200

    Added explicit conf file to the dhclient6_handler as OWRT seems to have some fixed conf file for dhclient which is hard to override with different file from different package.

--- openwrt/dhclient6_handler.sh
+++ openwrt/dhclient6_handler.sh
@@ -8,17 +8,18 @@
 # Copyright (c) 2012 cisco Systems, Inc.
 #
 # Created:       Fri Nov 16 13:10:54 2012 mstenber
-# Last modified: Fri Nov 16 13:13:18 2012 mstenber
-# Edit time:     1 min
+# Last modified: Wed Feb 27 13:53:27 2013 mstenber
+# Edit time:     3 min
 #
 
 CMD=$1
 IF=$2
 PIDFILE=$3
+CONFFILE=/etc/dhcp/dhclient6-hnet.conf
 
 case $CMD in
     start)
-        dhclient -6 -nw -P -pf $PIDFILE $IF
+        dhclient -6 -nw -P -cf $CONFFILE -pf $PIDFILE $IF
         ;;
     stop)
         kill `cat $PIDFILE`
