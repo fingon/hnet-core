c56685d21580b33e9679c7792acc9f3d7234990d
Markus Stenberg <markus.stenberg@iki.fi>
2013-06-20 18:10:57 +0300

    Made debugging in run_hp go to file also.

--- run_hp.sh
+++ run_hp.sh
@@ -8,8 +8,8 @@
 # Copyright (c) 2012 cisco Systems, Inc.
 #
 # Created:       Thu Sep 27 12:51:34 2012 mstenber
-# Last modified: Wed May 15 15:32:24 2013 mstenber
-# Edit time:     5 min
+# Last modified: Thu Jun 20 17:48:48 2013 mstenber
+# Edit time:     6 min
 #
 
 # Propagate LUA_PATH so that required modules can be found more easily..
@@ -17,7 +17,7 @@
 if [ "$1" = "-d" ]
 then
     shift
-    sudo ENABLE_MST_DEBUG=1 LUA_PATH=$LUA_PATH ./hp.lua $*
+    sudo ENABLE_MST_DEBUG=1 LUA_PATH=$LUA_PATH ./hp.lua $* 2>&1 | tee ~/hp.log
 else
     sudo LUA_PATH=$LUA_PATH ./hp.lua $*
 fi
