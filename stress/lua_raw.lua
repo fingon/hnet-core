3bea0e7e0c685eb51065eb6978f2c246ed84b2c7
Markus Stenberg <markus.stenberg@iki.fi>
2013-10-16 16:24:44 +0300

    Added some more core thing speed tests.

--- stress/lua_raw.lua
+++ stress/lua_raw.lua
@@ -8,8 +8,8 @@
 -- Copyright (c) 2013 cisco Systems, Inc.
 --
 -- Created:       Wed Oct 16 13:35:59 2013 mstenber
--- Last modified: Wed Oct 16 14:55:17 2013 mstenber
--- Edit time:     12 min
+-- Last modified: Wed Oct 16 16:17:31 2013 mstenber
+-- Edit time:     27 min
 --
 
 -- Raw lua performance testing (without busted dependency so that we
@@ -17,13 +17,27 @@
 
 require 'mst_test'
 
-local ptest = mst_test.perf_test:new_subclass{duration=1}
+local ptest = mst_test.perf_test:new_subclass{duration=5}
 
 local nop = ptest:new{cb=function ()
                          -- nop
                          end,
                       name='nop',
                      }
+
+ptest:new{cb=function ()
+             mst.d('x')
+end,
+          name='mst.d',
+          overhead={nop},
+         }:run()
+
+ptest:new{cb=function ()
+             mst.a(true)
+end,
+          name='mst.a',
+          overhead={nop},
+         }:run()
 
 local rnd = ptest:new{cb=function ()
                          mst.randint(1, 100)
@@ -32,7 +46,8 @@ local rnd = ptest:new{cb=function ()
                       overhead={nop},
                      }
 
-N_ARRAY_ITEMS=10000
+N_ARRAY_ITEMS=1000
+N_REMOVE=N_ARRAY_ITEMS/10
 
 local init_array=function ()
    local a = mst.array:new{}
@@ -43,16 +58,50 @@ local init_array=function ()
    return a
 end
 
+local init_iset=function ()
+   local a = mst.iset:new{}
+   for i=1,N_ARRAY_ITEMS
+   do
+      a:insert(i)
+   end
+   return a
+end
+
+-- test how fast remove_index (=table.remove) is
+
 local ia = ptest:new{cb=init_array,
-                     name='init_array'}
+                     name='init_array',
+                     overhead={nop}}
 
 local ra = ptest:new{cb=function ()
                         local a = init_array()
-                        a:remove_index(mst.randint(1, N_ARRAY_ITEMS))
+                        for i=1,N_REMOVE
+                        do
+                           a:remove_index(mst.randint(1, #a))
+                        end
                         end,
-                     name='remove_index',
-                     overhead={ia}}
+                     name='array remove_index+randint xN',
+                     overhead={nop, ia}}
 ra:run()
+
+-- test how fast remove (in iset) is
+
+local is = ptest:new{cb=init_iset,
+                     name='init_iset',
+                     overhead={nop}}
+
+local rs = ptest:new{cb=function ()
+                        local a = init_iset()
+                        for i=1,N_REMOVE
+                        do
+                           a:remove(a:randitem())
+                        end
+                        end,
+                     name='iset remove_index+randitem xN',
+                     overhead={nop, is}}
+rs:run()
+
+-- test how native fast table set/remove is
 
 local t = {}
 ptest:new{cb=function ()
